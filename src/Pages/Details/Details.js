1ad9b7303402c361f2986f2f61e70bb9ef97e35f
Tushar@8640 <imtantushar44@gmail.com>
2021-10-19 22:04:10 +0600

    dsgd

--- src/Pages/Details/Details.js
+++ src/Pages/Details/Details.js
@@ -28,7 +28,7 @@ const Details = () => {
                 
                 className="py-2 px-4  bg-gray-500 hover:bg-gray-700 focus:ring-gray-700 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
               >
-                Bye Now
+                Buy Now
               </button>
                 </div>
             </div>
